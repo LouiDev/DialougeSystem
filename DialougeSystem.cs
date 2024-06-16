da356f42344be5faf84af4e76ab88a98cff685ad
LouiDev <81969872+LouiDev@users.noreply.github.com>
2024-06-16 11:26:59 +0200

    initial commit

---
+++ DialougeSystem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LSPDFRCallout
+{
+    class DialougeSystem
+    {
+        private readonly String[] messages;
+        private readonly Dictionary<int, Action> indexActions;
+
+        private int index;
+
+        public DialougeSystem(String[] messages)
+        {
+            this.messages = messages;
+
+            index = -1;
+        }
+
+        public void Progress()
+        {
+            if (!HasFinished())
+            {
+                index++;
+            }
+
+            if (indexActions.TryGetValue(index, out Action action))
+            {
+                action.Invoke();
+            }
+        }
+
+        public void AddActionToIndex(int x, Action action)
+        {
+            indexActions.Add(x, action);
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public String GetCurrentMessage()
+        {
+            if (!IsRunning()) return "";
+            return messages[index];
+        }
+
+        public bool IsRunning()
+        {
+            return HasStarted() && !HasFinished();
+        }
+
+        public bool HasFinished()
+        {
+            return index >= messages.Length;
+        }
+
+        public bool HasStarted()
+        {
+            return index > -1;
+        }
+    }
+}
